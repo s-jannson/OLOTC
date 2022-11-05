601fd60f110b59961eb376342c7048c0f715c69b
Jansson <s.jansson@protonmail.com>
2022-11-05 14:04:40 +0000

    Added nginx conf

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.BASE_URL || 'http://localhost:5000/api/',
     proxy: true,
   },
 
